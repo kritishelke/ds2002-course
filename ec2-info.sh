d4f609a513d21e5bf72282ac068affcabce6dbeb
Kriti Shelke <kritishelke>
2026-04-01 13:15:26 -0400

    uploading files

--- ec2-info.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-aws ec2 describe-instances \
-  --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`]|[0].Value,ID:InstanceId,State:State.Name,Key:KeyName,PublicIP:PublicIpAddress}' \
-  --output table
